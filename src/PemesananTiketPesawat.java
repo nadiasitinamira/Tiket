3a04ecb049cfa003ed836be42ab40b591dc64eb5
ayakwae <39100835+ayakwae@users.noreply.github.com>
2019-05-06 13:18:44 +0700

    ea

--- src/PemesananTiketPesawat.java
+++ src/PemesananTiketPesawat.java
@@ -6,14 +6,14 @@
 
 /**
  *
- * @author USER
+ * @author ASUS
  */
-public class timet extends javax.swing.JFrame {
+public class PemesananTiketPesawat extends javax.swing.JFrame {
 
     /**
-     * Creates new form timet
+     * Creates new form PemesananTiketPesawat
      */
-    public timet() {
+    public PemesananTiketPesawat() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class timet extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(timet.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PemesananTiketPesawat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(timet.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PemesananTiketPesawat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(timet.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PemesananTiketPesawat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(timet.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PemesananTiketPesawat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new timet().setVisible(true);
+                new PemesananTiketPesawat().setVisible(true);
             }
         });
     }
